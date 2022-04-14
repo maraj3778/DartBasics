7ff01c867169762056e5c3fde41872d8c0dbf8a2
maraj3778 <marajsiddiki3778@gmail.com>
2022-04-14 09:19:12 +0600

    First commit

---
+++ basics.dart
@@ -0,0 +1,135 @@
+void main() {
+  // print("hello dart lang");
+//   //This function is used to print hello
+
+// //   this is
+// //   multiple line comment
+
+//   var name = "Maraj";
+//   var age = 25.56;
+//   var address = "224/6 block-k";
+//   var $money = 500;
+
+//   print(name);
+//   print(age);
+//   print(address);
+//   print ($money);
+
+//  String city = "Ranchi";
+//   print(city);
+
+//   bool is_completed = false;
+//     print(is_completed);
+
+//   bool is_login = true;
+//   print (is_login);
+
+//   var quantityz = 52.25;
+//   print (quantityz);
+//   print (quantityz.runtimeType);
+
+//   //check the type of the variable
+//  print(name.runtimeType);
+//   print(age.runtimeType);
+//   print(address.runtimeType);
+//   print($money.runtimeType);
+
+//   //arithmatic operator
+//   var a = 10;
+//   var b = 20;
+//   var c = 12;
+//   var d = 3;
+
+//   print (a+b);
+//   print (a-b);
+//   print (a/b);
+//   print (c-d);
+//   print (c ~/ d);
+
+// //   postfix and prefix
+
+//   var i = 3;
+
+//   print (--i);
+//   print (i);
+
+//   //equality and relational operation
+
+//     var as = 100;
+//     var bb = 200;
+//     print (a == b);
+//     print (a != b);
+//     print (a > b);
+//     print (a < b);
+//     print (a <= b);
+//     print (a >= b);
+
+//    var a = 100;
+//    var b = 200;
+//    var c = 300;
+//    var d = 200;
+
+//    print ((a == b) && (c == d));
+//    print ((a == b) || (d == b));
+
+// type test
+
+//    var name = "sonam";
+//    var number = 10;
+
+//    print (name is String);
+//    print (name is int);
+//    print (number is int);
+
+// Assignment Operator
+
+  // var n = 10;
+  // n += 5;
+  // n -= 6;
+  //n *= 5;
+
+  //  print (n);
+
+  // Multiline String
+
+//    var mltstr = """
+//    This is
+//    a Mulitiline string
+//    value""";
+//    print(mltstr);
+
+  //Expression inside string
+//    var name = "Maraj";
+//    print ('$name');
+//    print('${name}');
+//    print('My Name is $name');
+//    print("Please write here ${name.toUpperCase()}");
+//    print("Here is your name in lowercase ${name.toLowerCase()}");
+
+//    var city = "Gazipur";
+//    print("$city");
+//    print("Your home town is ${city.toUpperCase()}");
+
+  //String Concatenation
+//    print('Hello Dart');
+//    print('Hello' + ' ' + 'Dart' + ' ' + 'lang');
+
+//    var s1 = "Hello";
+//    var name = "Maraj";
+
+//    print(s1 + ' ' + 'This is' + ' ' + name);
+
+  //Raw String
+
+//    var address = r'Your address is \n in Gazipur chowrasta';
+//    print(address);
+
+  // var name = 'sonam';
+  // print(name.length);
+  // print(name.isEmpty);
+  // print(name.isNotEmpty);
+  // print(name.contains('n'));
+  // print(name.contains('x'));
+  // print(name.toUpperCase());
+  // print(name.padLeft(10));
+}
