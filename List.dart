3f0a6243bd8f2982f3901e9e8cf0b2f4dc2310bc
Maraj H Siddiki <49344613+maraj3778@users.noreply.github.com>
2022-04-14 14:45:42 +0600

    Add files via upload

---
+++ List.dart
@@ -0,0 +1,71 @@
+import 'dart:ffi';
+
+import 'dart:io';
+
+void main() {
+  // List names = ['Jones', 'Ruby', "Nasek", "Umara", "la haula", "Neerja"];
+  // List names1 = ['Manzi', 'The', 'Mountain', ...names];
+
+  // print(names);
+  // names.add('Marzia');
+  // print(names);
+  // names.removeAt(0);
+  // names.remove('Ruby');
+  // print(names);
+
+  // for (var name in names1) {
+  //   print(name);
+  //}
+  // var add = false;
+  // List name = ['Marry', 'Lava', for (var i = 1; i < 6; i++) 'Moto $i'];
+
+  // for (var names in name) {
+  //   print(names);
+  // }
+
+  // List lst = [1.1, 1.2, 1.3];
+  // print(lst);
+  // lst = ["Maraj", "Hossain", "Siddiki"];
+  // print(lst);
+
+  // lst[1] = "and";
+  // print(lst);
+  // lst.add('H');
+  // print(lst);
+  // lst.removeAt(0);
+  // print(lst);
+
+  // for (double i in lst) {
+  //   print(i);
+  // }
+  // List lsts = const [10, 20, 30];
+  // print(lsts);
+
+  // var lists = ["Maraj", "Maraf", "Marzia"];
+  // print(lists);
+  // print(lists[0]);
+  // print(lists[1]);
+  // print(lists[2]);
+
+  // print(lists.runtimeType);
+
+  // var lst2 = [1, 2, 3, 4];
+  // print(lst2);
+  // print(lst2.runtimeType);
+
+  // var lst3 = [1, 2, 3, "Hello"];
+  // print(lst3);
+  // print(lst3.runtimeType);
+
+  //var women = ["Marzia", "Kulsum", "Hasna"];
+  // var men = ["Arjun", "Rampal", "Akshay"];
+  // var menwillbemen = ["Luchu", ...men];
+  // var womenempower = ["mina", 'shushi', ...women];
+  // print(womenempower);
+  // print(menwillbemen);
+
+  // var student = [];
+  // student.add("Maraj");
+  // print(student);
+  // print(student.runtimeType);
+}
