460bf5be4b9638c5ffcb740abc2fbce81e3cbcb4
Maraj H Siddiki <49344613+maraj3778@users.noreply.github.com>
2022-04-19 21:25:23 +0600

    Add files via upload

---
+++ map.dart
@@ -0,0 +1,45 @@
+void main(List<String> args) {
+  // var address = {'Name': 'Maraj', 'City': "Gazipur", 'Zip': 120};
+  // print(address);
+  // print(address.runtimeType);
+  // Map<String, String> address = {
+  //   'Name': "Maraf",
+  //   'Age': "25",
+  //   'state': "Patuakhali"
+  // };
+
+  // print(address);
+  // print(address.runtimeType);
+
+  // var product = {10: 'mobile', 20: 'phone'};
+  // print(product);
+  // print(product.runtimeType);
+
+  // var address = Map();
+  // print(address);
+  // print(address.runtimeType);
+
+  // Map<String, String> newaddress = Map();
+  // print(newaddress);
+  // print(newaddress.runtimeType);
+
+  // add item in the map
+  // var address2 = {};
+  // address2['name'] = "Maraj";
+  // address2['age'] = "25";
+  // address2['city'] = "Joydebpur";
+
+  // print(address2);
+  // print(address2['age']);
+  // print(address2['city']);
+  // print(address2.runtimeType);
+
+  // var address5 = {'name': "Maraj", 'age': "25", 'state': "Jaydevpur"};
+  // print(address5.length);
+  // print(address5.isEmpty);
+  // print(address5.isNotEmpty);
+  // print(address5.keys);
+  // print(address5.values);
+  // print(address5.containsKey('namoe'));
+  // print(address5.containsValue('Maraj'));
+}
